7c57bddc2abf44082300eb4b541af33b42825b52
Piero Angulo Cubas <pieroangulocubas.17@gmail.com>
2024-06-13 18:32:01 +0200

    Creacion de script para reservar cita de DNI y Pasaporte

--- scripts/dni.js
+++ scripts/dni.js
@@ -7,12 +7,11 @@ async function main(){
     let isDeleted=true
     while(isDeleted){
         try{
+            await delay(250)
             const pasaporteTimes=await obtenerFechasYHorariosDisponiblesPorTramite(servicePasaporteName)
             const timesForReserved=pasaporteTimes.slice(0,15)
-            console.log(timesForReserved)
-            if(timesForReserved.length<1){
-                continue
-            }
+            if(timesForReserved.length<6) continue
+
             const reservedAppointments = await bloquearHorarios(servicePasaporteName,timesForReserved)
             isDeleted=false
             console.log(reservedAppointments.length)
--- scripts/pasaporte.js
+++ scripts/pasaporte.js
@@ -7,22 +7,18 @@ async function main(){
     let isDeleted=true
     while(isDeleted){
         try{
+            await delay(100)
             const pasaporteTimes=await obtenerFechasYHorariosDisponiblesPorTramite(servicePasaporteName)
             const timesForReserved=pasaporteTimes.slice(0,15)
-            if(!timesForReserved.length>=8){
-                continue
-            }
+            if(timesForReserved.length<2) continue
+
             const reservedAppointments = await bloquearHorarios(servicePasaporteName,timesForReserved)
             isDeleted=false
             console.log(reservedAppointments.length)
-            if(reservedAppointments.length>=2){
-                await delay(timeoutForDelete)
-                const result = await deleteAppointments(reservedAppointments)
-                isDeleted=true
-                console.log("Citas Eliminadas")
-            }else{
-                isDeleted=true
-            }
+            await delay(timeoutForDelete)
+            await deleteAppointments(reservedAppointments)
+            console.log("Citas Eliminadas")
+            isDeleted=true
         }catch(error){
             //console.log(error.message)
         }
